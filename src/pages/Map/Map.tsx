4f49f1995bac71a3a5ff711f249f6b5235d507cf
yuotuo <stemplah@gmail.com>
2026-08-26 16:50:26 -0700

    updated viewbox dimensions for mobile

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -119,7 +119,7 @@ function Map() {
     <div>
       <div className="mt-0 md:mt-[calc(5rem-10vh)] h-[50vh] relative w-screen overflow-visible rounded-xl flex items-center justify-center flex-row">
         <div className="w-[90vw] md:w-[50vw] max-w-6xl aspect-[4/3] relative overflow-visible rounded-xl ml-auto mr-auto">
-          <h1 className="text-2xl font-bold text-black drop-shadow font-cascadia block text-center absolute left-1/2 -translate-x-1/2 z-10 pointer-events-none bottom-full mb-2 w-full md:bottom-auto md:top-0 md:mb-0">
+          <h1 className="text-2xl font-bold text-black drop-shadow font-cascadia block text-center absolute left-1/2 -translate-x-1/2 z-10 pointer-events-none bottom-full w-full md:bottom-auto md:top-0 md:mb-0">
             GDCC's Club Count in the US: {locations.length}
           </h1>
           <div
@@ -135,7 +135,7 @@ function Map() {
           >
             <ComposableMap
               projection="geoAlbersUsa"
-              viewBox="0 0 800 600"
+              viewBox="0 120 800 400"
               className="w-full h-full overflow-visible"
               onClick={() => {
                 if (activeState === null) return;
